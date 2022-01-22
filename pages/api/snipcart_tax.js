88eeb95cd3554d1aea9c2c9fef2646f9a1c990fd
Jason-Kyle De Lara <jasondl@icpdas-usa.com>
2022-01-21 17:01:55 -0800

    taxes

--- pages/api/snipcart_tax.js
+++ pages/api/snipcart_tax.js
@@ -1,67 +1,43 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-// import Papa from "papaparse"
-// // import csv from "../../public/tax_rates/TAXRATES.csv"
-// // var data = Papa.parse(csv);
-// // const reader = new FileReader()
-// var data = Papa.parse("http://localhost:3000/tax_rates/TAXRATES.csv", {
-// 	download: true,
-// 	complete: function(results) {
-// 		console.log(results);
-// 	}
-// });
-// console.log(data);
-// require csvtojson module
-const CSVToJSON = require('csvtojson');
-
-// convert users.csv file to JSON array
-
-const getJson = async () => {
-    CSVToJSON().fromFile('TAXRATES.csv')
-    .then(taxes => {
-
-        // users is a JSON array
-        // log the JSON array
-         return taxes;
-    }).catch(err => {
-        // log error if any
-        console.log(err);
-    });
-}
+import { fetchAPI } from "../../utils/api"
 
 export default function handler(req, res) {
-    let taxes;
-    // console.log(getJson)
-    if (req.method === 'POST') {
+    let data = fetchAPI('http://localhost:3000/api/taxes_json');
+    console.log(data)
+
+    let taxes
+  // console.log(getJson)
+  if (req.method === "POST") {
     // Process a POST request
-        console.log(req.body)
-        let data = {
-            name: req.body.content.shippingAddress.postalCode,
-            postalCodeMatchingRegex: req.body.content.shippingAddress.postalCode,
-            rate: 0.05
-        }
-        taxes = data;
-    } else {
-    // Handle any other HTTP method
+    console.log(req.body)
+    if{}
+    let data = {
+      name: req.body.content.shippingAddress.postalCode,
+      postalCodeMatchingRegex: req.body.content.shippingAddress.postalCode,
+      rate: 0.05,
     }
-    // taxes[0].rate = 0.3;
-    res.status(200).json({
-    taxes
-  })
+    taxes = data
+  } else {
+    // Handle any other HTTP method
+  }
+  // taxes[0].rate = 0.3;
   res.status(200).json({
-    "taxes": [
-      {
-        "name": "Tax1",
-        "amount": 10.00,
-        "numberForInvoice": "TAX-001",
-        "rate": 0.05
-      },
-      {
-        "name": "Tax2",
-        "amount": 20.00,
-        "numberForInvoice": "TAX-002",
-        "rate": 0.10
-      }
-    ]
+    taxes,
   })
+  //   res.status(200).json({
+  //     "taxes": [
+  //       {
+  //         "name": "Tax1",
+  //         "amount": 10.00,
+  //         "numberForInvoice": "TAX-001",
+  //         "rate": 0.05
+  //       },
+  //       {
+  //         "name": "Tax2",
+  //         "amount": 20.00,
+  //         "numberForInvoice": "TAX-002",
+  //         "rate": 0.10
+  //       }
+  //     ]
+  //   })
 }
---
+++ pages/api/taxes_json.js
@@ -0,0 +1,16 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+const CSVToJSON = require('csvtojson');
+export default function handler(req, res) {
+    CSVToJSON().fromFile('TAXRATES.csv')
+    .then(taxes => {
+        res.status(200).json({
+            taxes
+          })
+    }).catch(err => {
+        // log error if any
+        res.status(200).json({
+            err: "Error"
+          })
+    });
+}
